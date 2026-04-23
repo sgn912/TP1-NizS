08a6901852d360810e84e1af8545d05012cae85c
sgn912 <santiagoniz4@gmail.com>
2026-04-23 13:53:33 -0300

    Create Ejercicio6.java

---
+++ Ejercicio6.java
@@ -0,0 +1,27 @@
+package resolNizS;
+
+import java.util.Random;
+import java.util.Scanner;
+
+public class Ejercicio6 {
+    public static void ejecutar() {
+        Random r = new Random();
+        Scanner sc = new Scanner(System.in);
+
+        int numero = r.nextInt(100) + 1;
+        int intento;
+
+        do {
+            System.out.print("Adivina el número (1-100): ");
+            intento = sc.nextInt();
+
+            if (intento > numero)
+                System.out.println("Más bajo");
+            else if (intento < numero)
+                System.out.println("Más alto");
+
+        } while (intento != numero);
+
+        System.out.println("¡Correcto!");
+    }
+}
