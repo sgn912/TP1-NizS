7518f789e9a357c5fcb49331d6a03af107881993
sgn912 <santiagoniz4@gmail.com>
2026-04-23 14:12:44 -0300

    Update Ejercicio1.java

--- Ejercicio1.java
+++ Ejercicio1.java
@@ -3,7 +3,7 @@ package resolNizS;
 public class Ejercicio1 {
     public static void ejecutar(String[] args) {
         if (args.length == 0) {
-            System.out.println("Debe ingresar un nombre");
+            System.out.println("Ingresa un nombre");
             return;
         }
         System.out.println("Hola " + args[0] + "!");
