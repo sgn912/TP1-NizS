a1b4cb8f873dc13ac242170b48b0b4db660894c2
sgn912 <santiagoniz4@gmail.com>
2026-04-23 14:10:26 -0300

    Update Ejercicio5.java

--- Ejercicio5.java
+++ Ejercicio5.java
@@ -5,15 +5,15 @@ import java.util.Scanner;
 public class Ejercicio5 {
     public static void ejecutar() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ingrese una palabra: ");
+        System.out.print("Ingresa una palabra: ");
         String palabra = sc.nextLine();
 
         String invertida = new StringBuilder(palabra).reverse().toString();
 
         if (palabra.equalsIgnoreCase(invertida)) {
-            System.out.println("Es palíndromo");
+            System.out.println("Es palindromo");
         } else {
-            System.out.println("No es palíndromo");
+            System.out.println("No es palindromo");
         }
     }
 }
