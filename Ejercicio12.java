f6dd21df2476f64e8d350d5a104450d1d5212be9
sgn912 <santiagoniz4@gmail.com>
2026-04-23 14:00:58 -0300

    Create Ejercicio12.java

---
+++ Ejercicio12.java
@@ -0,0 +1,23 @@
+package resolNizS;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Ejercicio12 {
+    public static void ejecutar() {
+        Scanner sc = new Scanner(System.in);
+        int[] nums = new int[5];
+
+        for (int i = 0; i < 5; i++) {
+            System.out.print("Ingrese número " + (i + 1) + ": ");
+            nums[i] = sc.nextInt();
+        }
+
+        Arrays.sort(nums);
+
+        System.out.println("Ordenados de mayor a menor:");
+        for (int i = nums.length - 1; i >= 0; i--) {
+            System.out.print(nums[i] + " ");
+        }
+    }
+}
