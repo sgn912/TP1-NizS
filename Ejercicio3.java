6563725e807b28e1835fd69380046685cf7b834e
sgn912 <santiagoniz4@gmail.com>
2026-04-23 14:11:16 -0300

    Update Ejercicio3.java

--- Ejercicio3.java
+++ Ejercicio3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Ejercicio3 {
     public static void ejecutar() {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Ingrese un número: ");
+        System.out.print("Ingresa un numero: ");
         int n = sc.nextInt();
 
         for (int i = 1; i <= 10; i++) {
